6d01dcb90a4bb61ebb334ab32c8f434f88693381
Shashank Rustagi <mathisfunlike321@gmail.com>
2021-05-24 18:20:57 +0530

    commit

--- Codeforces/610a.cpp
+++ Codeforces/610a.cpp
@@ -7,6 +7,10 @@ int main(){
     while(t--){
         long long int n;
         cin>>n;
+        if(n<6){
+            cout<<0;
+            break;
+        }
         n=n/2;
         long long int l=1;
         long long int r = n-1;
