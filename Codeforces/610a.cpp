05f37f625d833b2eda4d3ac72bf0c3a4470d7023
Shashank Rustagi <mathisfunlike321@gmail.com>
2021-05-24 18:24:13 +0530

    commit

--- Codeforces/610a.cpp
+++ Codeforces/610a.cpp
@@ -1,20 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int main(){
     int t;
     cin>>t;
     while(t--){
-        long long int n;
+        long int n;
         cin>>n;
-        if(n<6){
+        if(n<6 || n%2){
             cout<<0;
             break;
         }
         n=n/2;
-        long long int l=1;
-        long long int r = n-1;
-        long long int count =0;
+        long int l=1;
+        long int r = n-1;
+        int count =0;
         while(l<n and r>n){
             l++;
             r--;
