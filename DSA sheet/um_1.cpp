781b9e1665565dc05b8d5411b75695e0bb394ffd
Shashank Rustagi <mathisfunlike321@gmail.com>
2021-05-24 10:51:07 +0530

    commit

---
+++ DSA sheet/um_1.cpp
@@ -0,0 +1,21 @@
+//Given a string of words, find the frequency of words in the string.
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+void printfrequencies(const string &str){
+    unordered_map<string,int> m;
+    stringstream ss(str);
+    string word;
+    while(ss >> word){
+        m[word]++;
+        unordered_map<string,int>:: iterator it;
+        for(it = m.begin();it!=m.end();it++)
+            cout << it->first << "   "  << it->second <<endl;
+        
+    }
+}
+int main(){
+    string strs = "Shashank Shashank Rustagi";
+    printfrequencies(strs);
+    return 0;
+}
