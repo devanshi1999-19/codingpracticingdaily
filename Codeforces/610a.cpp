550a2b1fd2edcf23c99026133fa761b5d4aee2b9
Shashank Rustagi <mathisfunlike321@gmail.com>
2021-05-24 18:10:47 +0530

    commit

---
+++ Codeforces/610a.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long int n;
+        cin>>n;
+        n=n/2;
+        long long int l=1;
+        long long int r = n-1;
+        long long int count =0;
+        while(l<n and r>n){
+            l++;
+            r--;
+            count++;
+        }
+        cout<<count<<"\n";
+    }
+    return 0;
+}
