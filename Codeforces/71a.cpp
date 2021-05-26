de5e8b2e760169cd33c3fa4332804d0b5218a689
Shashank Rustagi <mathisfunlike321@gmail.com>
2021-05-27 00:31:11 +0530

    commit

---
+++ Codeforces/71a.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        if(s.size()>10)
+        cout<<s[0]<<s.size()-2<<s[s.size()-1];
+        else
+        cout<<s;
+
+        cout<<"\n";
+    }
+    return 0;
+}
