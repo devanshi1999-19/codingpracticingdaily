169aacea92dc4dc5846351e7d9bb7909b820c431
Shashank Rustagi <mathisfunlike321@gmail.com>
2021-06-24 16:39:29 +0530

    commit

--- special codes/subsetsumcount.cpp
+++ special codes/subsetsumcount.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int subsetsum(int a[],int n,int sum)
 {
     int dp[n+1][sum+1];
-    for(int i=0;i<=sum;i++)
-        dp[0][i]=0;
     for(int j=0;j<=n;j++)
         dp[j][0]=1;
+    for(int i=0;i<=sum;i++)
+        dp[0][i]=0;
+    
+    dp[0][0]=1;
     
     for(int i=1;i<=n;i++)
     {
