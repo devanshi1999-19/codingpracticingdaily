9f076610556df6892c87f8cf9189db59d5a21238
Shashank Rustagi <mathisfunlike321@gmail.com>
2021-06-28 02:04:02 +0530

    commit

---
+++ CodeChef/tictacto.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    
+    while(t--)
+    {
+       int n,a,b;
+       cin>>n>>a>>b;
+       int r;
+       r = (2*(180+n)-a-b);
+       cout<<r<<endl;
+
+    }
+    return 0;
+}
